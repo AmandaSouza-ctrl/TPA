a549a9936642fa42b3617703775e8d8ae2c6a9dd
Amanda Souza <55111387+AmandaSouza-ctrl@users.noreply.github.com>
2022-04-30 21:04:42 -0300

    Add files via upload

---
+++ EdgeType.java
@@ -0,0 +1,15 @@
+package core;
+
+public class EdgeType {
+	    private float weight; 	
+	    public float getWeight() {
+		return weight;
+	}
+	public void setWeight(float weight) {
+		this.weight = weight;
+	}
+		
+	    public EdgeType(float w) {
+	    	this.weight = w;
+	    }
+	}
